924de9b8e597804e76cc1df5e92bfa07d87d0d61
Vince Gerard L. Payot <130125292+vincesuu@users.noreply.github.com>
2025-01-31 12:50:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! I loveyouuuu My Zeelato<3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
+    "zeelato please",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
